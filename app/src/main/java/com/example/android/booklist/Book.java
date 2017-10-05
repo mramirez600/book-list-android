60f36c81b26020a1f17458dbb6ae02e2fb0cdde2
Marvin Ramirez <mramirez@constantevolve.ca>
2017-10-05 06:14:26 -0400

    project commit

---
+++ app/src/main/java/com/example/android/booklist/Book.java
@@ -0,0 +1,56 @@
+package com.example.android.booklist;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by marvinramirez on 2017-10-05.
+ */
+
+public class Book implements Parcelable {
+
+    String author;
+    String title;
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    protected Book(Parcel in) {
+        author = in.readString();
+        title = in.readString();
+    }
+
+    public static final Creator<Book> CREATOR = new Creator<Book>() {
+        @Override
+        public Book createFromParcel(Parcel in) {
+            return new Book(in);
+        }
+
+        @Override
+        public Book[] newArray(int size) {
+            return new Book[size];
+        }
+    };
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(author);
+        parcel.writeString(title);
+    }
+}
